86bb9fb38f86c1247c557a151c81f3750172110a
DMitong <mitongdapal@gmail.com>
2023-12-17 11:44:39 +0100

    made minor updates

--- src/MRetriever.sol
+++ src/MRetriever.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.21;
 
 // Challenge: This is part of a 15 day smart contract writing challenge, with 5 days for each level of difficulty, basic, intermediate and advanced.
 
-// Basic 1:
-// Write a smart contract that can store and retrieve a message.
-// The message should be a string variable that can be set by the contract owner or anyone who calls the contract.
-// The contract should also have a function to get the current message.
+/*
+Basic 1:
+Write a smart contract that can store and retrieve a message.
+The message should be a string variable that can be set by the contract owner or anyone who calls the contract.
+The contract should also have a function to get the current message.
+*/
 
 contract MessageRetriever {
     string private message;
